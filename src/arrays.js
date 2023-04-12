e04844a3db021b3292f4b02cad93610e37aafb1b
Utsab Bera <nCoder24@users.noreply.github.com>
2023-04-12 12:32:46 +0530

    fixed the linkings with arrays tests, src and testing

--- src/arrays.js
+++ src/arrays.js
@@ -1,20 +1,25 @@
+const isEven = function(number) {
+  return number % 2 === 0;
+}
+
 const selectEvenNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isEven(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (isEven(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
 const selectOddNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isOdd(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (isOdd(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
+exports.selectEvenNumbers = selectEvenNumbers;
--- test/arrays-test.js
+++ test/arrays-test.js
@@ -1,6 +1,10 @@
+const test = require("../lib/testing.js");
+const array = require("../src/arrays.js");
+
+const selectEvenNumbers = array.selectEvenNumbers;
 
 const testSelectEvenNumbers = function() {
-	test.assertListsEqual([2, 4, 6], selectEvenNumbers([1,2,3,4,5,6,7]), "Select even numbers");
+  test.assertListsEqual([2, 4, 6], selectEvenNumbers([1,2,3,4,5,6,7]), "Select even numbers");
 }
 
 testSelectEvenNumbers();
